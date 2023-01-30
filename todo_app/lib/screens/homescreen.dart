e41926d5e4e7285a47bc7fb0699e0a8ad4dd842e
snowstorm47 <contact.abelendeshaw@gmail.com>
2023-01-30 21:45:26 +0300

    added the app bar for the app, and colors and refactored app bar

--- todo_app/lib/screens/homescreen.dart
+++ todo_app/lib/screens/homescreen.dart
@@ -14,30 +14,35 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: tdBGColor,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Icon(
-              Icons.menu,
-              color: tdBlack,
-              size: 30,
-            ),
-            Container(
-              height: 40,
-              width: 40,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Image.asset("assets/images/avatar.jpg"),
-              ),
-            )
-          ],
-        ),
-      ),
+      backgroundColor: tdBGColor,
+      appBar: _buildAppBar(),
       body: Column(
         children: [],
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: tdBGColor,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset("assets/images/avatar.jpg"),
+            ),
+          )
+        ],
       ),
     );
   }
